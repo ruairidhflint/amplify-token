702a1c7c88856d24dd2d6f3fedf2d2263bab323e
Rory Flint <roryflintphoto@gmail.com>
2022-08-19 07:19:13 +0100

    chore: add linting and formatting scripts

--- bin/index.js
+++ bin/index.js
@@ -1,57 +1,57 @@
 #! /usr/bin/env node
-import Configstore from "configstore";
-import { Command } from "commander";
-import prompt from "prompt";
-import { Auth } from "@aws-amplify/auth";
-import chalk from "chalk";
+import Configstore from 'configstore'
+import { Command } from 'commander'
+import prompt from 'prompt'
+import { Auth } from '@aws-amplify/auth'
+import chalk from 'chalk'
 
-const program = new Command();
-const config = new Configstore("amplify-token");
+const program = new Command()
+const config = new Configstore('amplify-token')
 
 program
-  .command("config")
+  .command('config')
   .description(
-    "Set and view the configuration settings for your AWS Amplify Request"
+    'Set and view the configuration settings for your AWS Amplify Request'
   )
   .option(
-    "--set [values...]",
-    "Set a config value. Takes 2 arguments (key and value)"
+    '--set [values...]',
+    'Set a config value. Takes 2 arguments (key and value)'
   )
-  .option("--get [value]", "Get a config value. Takes 1 arguments (key)")
-  .option("--clear", "Clear config of all values")
-  .option("--get-all", "View all values in config file")
+  .option('--get [value]', 'Get a config value. Takes 1 arguments (key)')
+  .option('--clear', 'Clear config of all values')
+  .option('--get-all', 'View all values in config file')
   .action(function (options) {
     if (Object.keys(options).length === 0) {
       return console.log(
         chalk.yellow(
-          "You must supply additional arguments to the config command. Enter rory config --help to view all available commands"
+          'You must supply additional arguments to the config command. Enter rory config --help to view all available commands'
         )
-      );
+      )
     }
     if (Object.keys(options).length > 1) {
       return console.log(
         chalk.yellow(
-          "You can only specify one command at a time. Enter rory config --help to view all available commands"
+          'You can only specify one command at a time. Enter rory config --help to view all available commands'
         )
-      );
+      )
     }
     if (options.set) {
       if (options.set.length !== 2) {
         return console.log(
           chalk.yellow(
-            "Setting a config variable requires both a key and a value eg. region us-west"
+            'Setting a config variable requires both a key and a value eg. region us-west'
           )
-        );
+        )
       }
-      config.set(options.set[0], options.set[1]);
-      return;
+      config.set(options.set[0], options.set[1])
+      return
     }
     if (options.get) {
-      const retrieved = config.get(options.get);
+      const retrieved = config.get(options.get)
       if (!retrieved) {
-        return console.log(chalk.red(`No key ${options.get} exists`));
+        return console.log(chalk.red(`No key ${options.get} exists`))
       } else {
-        return console.log(chalk.green(retrieved));
+        return console.log(chalk.green(retrieved))
       }
     }
     if (options.clear) {
@@ -60,7 +60,7 @@ program
           properties: {
             Clear: {
               description: chalk.yellow(
-                "Are you sure you want to clear all config settings? y/n"
+                'Are you sure you want to clear all config settings? y/n'
               ),
               required: true,
             },
@@ -68,28 +68,28 @@ program
         },
         function (err, result) {
           if (err) {
-            return 1;
+            return 1
           }
-          if (result.Clear.toLowerCase() === "y" || result.Clear === "yes") {
-            console.log(chalk.red("Configuration cleared"));
-            return config.clear();
+          if (result.Clear.toLowerCase() === 'y' || result.Clear === 'yes') {
+            console.log(chalk.red('Configuration cleared'))
+            return config.clear()
           }
         }
-      );
+      )
     }
     if (options.getAll) {
-      return console.log(config.all);
+      return console.log(config.all)
     }
-  });
+  })
 
 program
-  .command("fetch")
-  .description("Fetch JWT with email and password")
+  .command('fetch')
+  .description('Fetch JWT with email and password')
   .action(function () {
     Auth.configure({
       Auth: config.all,
-    });
-    prompt.start();
+    })
+    prompt.start()
     prompt.get(
       {
         properties: {
@@ -104,21 +104,21 @@ program
       },
       async function (err, result) {
         if (err) {
-          console.dir(err);
-          return 1;
+          console.dir(err)
+          return 1
         }
         try {
-          console.log(chalk.yellow("Attempting to fetch token..."));
-          const user = await Auth.signIn(result.email, result.password);
+          console.log(chalk.yellow('Attempting to fetch token...'))
+          const user = await Auth.signIn(result.email, result.password)
           console.log(
             chalk.blue(`\n\n\n${user.signInUserSession.idToken.jwtToken}\n\n\n`)
-          );
+          )
         } catch (error) {
-          console.dir(error);
-          return 1;
+          console.dir(error)
+          return 1
         }
       }
-    );
-  });
+    )
+  })
 
-program.parse(process.argv);
+program.parse(process.argv)
